44aefb1eefaf5e22be59ce69b82a6ac630cb6031
Tarık KAYA <tiqqq24@gmail.com>
2023-11-09 19:48:53 +0300

    discoveredMovie initialstate changed

--- src/stores/movie/index.ts
+++ src/stores/movie/index.ts
@@ -13,7 +13,7 @@ const initialState: initialStateType = {
   discoveredMovie: {
     adult: false,
     backdrop_path:
-      "https://image.tmdb.org/t/p/original/aQPeznSu7XDTrrdCtT5eLiu52Yu.jpg",
+      "https://image.tmdb.org/t/p/original/fiVW06jE7z9YnO4trhaMEdclSiC.jpg",
     genre_ids: [28, 53],
     id: 575264,
     original_language: "en",
@@ -22,7 +22,7 @@ const initialState: initialStateType = {
       "Ethan Hunt and his IMF team embark on their most dangerous mission yet: To track down a terrifying new weapon that threatens all of humanity before it falls into the wrong hands. With control of the future and the world's fate at stake and dark forces from Ethan's past closing in, a deadly race around the globe begins. Confronted by a mysterious, all-powerful enemy, Ethan must consider that nothing can matter more than his mission—not even the lives of those he cares about most.",
     popularity: 3264.457,
     poster_path:
-      "https://image.tmdb.org/t/p/w500/NNxYkU70HPurnNCSiCjYAmacwm.jpg",
+      "https://image.tmdb.org/t/p/w500/fiVW06jE7z9YnO4trhaMEdclSiC.jpg",
     release_date: "2023-07-08",
     title: "Mission: Impossible - Dead Reckoning Part One",
     video: false,
