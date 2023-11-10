5a4302841160dca715c62b0c3906fc45a85a89dc
Tarık KAYA <tiqqq24@gmail.com>
2023-11-10 18:03:17 +0300

    div100h library added for mobile design

--- src/layouts/web/index.tsx
+++ src/layouts/web/index.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
+import Div100vh from "react-div-100vh";
 
 const WebLayout = () => {
   return (
-    <div className="h-screen w-full mx-auto flex flex-col p-5 relative">
+    <Div100vh className="h-screen w-full mx-auto flex flex-col p-5 relative">
       <Header />
       <main className="absolute top-0 left-0 w-full h-screen">
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </Div100vh>
   );
 };
 
