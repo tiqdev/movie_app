4aefd597e4971c3a3adcdc8751d202a584d54f9b
Tarık KAYA <tiqqq24@gmail.com>
2023-11-11 00:29:08 +0300

    navbar padding issue solved

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,8 +3,8 @@ import LogoButton from "./logo_button";
 
 const Header = () => {
   return (
-    <header className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-20 px-4 flex bg-overlay_pattern_dark_top z-20">
-      <nav className="flex w-full max-w-[1280px] items-center mx-auto justify-between">
+    <header className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-20  flex bg-overlay_pattern_dark_top z-20">
+      <nav className="flex w-full max-w-[1280px] px-4 items-center mx-auto justify-between">
         <LogoButton />
         <GoogleLogin />
       </nav>
