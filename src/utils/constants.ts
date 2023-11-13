11b54f0f4e2deeedec2dd8bd3d6d15ee51c17e84
Tarık KAYA <tiqqq24@gmail.com>
2023-11-13 15:32:53 +0300

    vercel env file issue fixed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,11 +6,11 @@ export const w780ImageUrl = "https://image.tmdb.org/t/p/w780";
 export const _token = process.env.TMDB_API_TOKEN;
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDwBLz9LA1cntJkLLLoZXdrhmuYzLLCLig",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "tiqdevmovie.firebaseapp.com",
   projectId: "tiqdevmovie",
   storageBucket: "tiqdevmovie.appspot.com",
-  messagingSenderId: "53076343629",
-  appId: "1:53076343629:web:77310c482e780e2c1a5504",
-  measurementId: "G-E5GJTTGCMK",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
